47a1051f6911e94be488355563e145f43b5e7729
Donny Winston <dwinston@alum.mit.edu>
2018-10-09 13:39:53 -0400

    Add package for data import

---
+++ jupyterworkflow/data.py
@@ -0,0 +1,29 @@
+import os
+from urllib.request import urlretrieve
+import pandas as pd
+
+FREMONT_URL = 'https://data.seattle.gov/api/views/65db-xm6k/rows.csv?accessType=DOWNLOAD'
+
+def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL,
+                     force_download=False):
+    """Download and cache Fremont bike data.
+
+    Uses pandas to format timestamps and add column labeling to return
+    a dataframe ready for analysis.
+    
+    Args:
+        filename (str): CSV file of data
+        url (str): URL to retrieve file if not already downloaded
+        force_download (bool): Force (re-)download of file.
+        
+    Returns:
+        pandas.DataFrame: Fremont bike counter data
+ 
+    """
+    if force_download or not os.path.exists(filename):
+        # You can now download the data from the database
+        urlretrieve(url, filename)
+    data = pd.read_csv(filename, index_col='Date', parse_dates=True)
+    data.columns = ['East', 'West']
+    data['Total'] = data['West'] + data['East']
+    return data
